2e962e2db579bd8c286e457dfeb8b8f3a5ee288f
TheBigSot <sot_c2@hotmail.com>
2015-02-21 11:58:05 -0400

    Added Cost Chart

---
+++ RulesetView/Models/Item.cs
@@ -0,0 +1,13 @@
+﻿using YamlDotNet.Serialization;
+
+namespace RulesetView.Models
+{
+    public class Item
+    {
+        [YamlMember(Alias = "type")]
+        public string Name { get; set; }
+
+        [YamlMember(Alias = "costSell")]
+        public decimal SellPrice { get; set; }
+    }
+}
