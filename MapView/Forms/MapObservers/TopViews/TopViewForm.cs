7d699bf99ad76552c5881d6fcebea148ca67773e
kevL <k3v1s73n@gmail.com>
2016-12-29 21:50:34 -0700

    format ....

--- MapView/Forms/MapObservers/TopViews/TopViewForm.cs
+++ MapView/Forms/MapObservers/TopViews/TopViewForm.cs
@@ -2,13 +2,19 @@
 
 namespace MapView.Forms.MapObservers.TopViews
 {
-	public partial class TopViewForm : Form, IMapObserverFormProvider
+	public partial class TopViewForm
+		:
+		Form,
+		IMapObserverFormProvider
 	{
 		public TopViewForm()
 		{
 			InitializeComponent();
 		}
 
-		public MapObserverControl MapObserver { get { return TopViewControl; } }
+		public MapObserverControl MapObserver
+		{
+			get { return TopViewControl; }
+		}
 	}
 }
