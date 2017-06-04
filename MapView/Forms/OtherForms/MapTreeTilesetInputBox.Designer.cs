4d2fb7542a4123647f23ce7b90ff7c082c71107b
kevL <k3v1s73n@gmail.com>
2017-06-03 19:35:55 -0600

    seems like it's working.

--- MapView/Forms/OtherForms/MapTreeTilesetInputBox.Designer.cs
+++ MapView/Forms/OtherForms/MapTreeTilesetInputBox.Designer.cs
@@ -75,7 +75,7 @@
 			// btnOk
 			// 
 			this.btnOk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-			this.btnOk.Location = new System.Drawing.Point(415, 0);
+			this.btnOk.Location = new System.Drawing.Point(435, 0);
 			this.btnOk.Name = "btnOk";
 			this.btnOk.Size = new System.Drawing.Size(80, 25);
 			this.btnOk.TabIndex = 1;
@@ -86,7 +86,7 @@
 			// 
 			this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
 			this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-			this.btnCancel.Location = new System.Drawing.Point(505, 0);
+			this.btnCancel.Location = new System.Drawing.Point(525, 0);
 			this.btnCancel.Name = "btnCancel";
 			this.btnCancel.Size = new System.Drawing.Size(80, 25);
 			this.btnCancel.TabIndex = 2;
@@ -107,7 +107,7 @@
 			this.tbTileset.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
 			this.tbTileset.Location = new System.Drawing.Point(50, 35);
 			this.tbTileset.Name = "tbTileset";
-			this.tbTileset.Size = new System.Drawing.Size(450, 19);
+			this.tbTileset.Size = new System.Drawing.Size(470, 19);
 			this.tbTileset.TabIndex = 4;
 			this.tbTileset.TextChanged += new System.EventHandler(this.OnTilesetLabelChanged);
 			this.tbTileset.KeyUp += new System.Windows.Forms.KeyEventHandler(this.OnTilesetKeyUp);
@@ -120,7 +120,7 @@
 			this.pnlBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
 			this.pnlBottom.Location = new System.Drawing.Point(0, 426);
 			this.pnlBottom.Name = "pnlBottom";
-			this.pnlBottom.Size = new System.Drawing.Size(592, 30);
+			this.pnlBottom.Size = new System.Drawing.Size(612, 30);
 			this.pnlBottom.TabIndex = 1;
 			// 
 			// lblAddType
@@ -139,7 +139,7 @@
 			this.pnlTop.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.pnlTop.Location = new System.Drawing.Point(0, 0);
 			this.pnlTop.Name = "pnlTop";
-			this.pnlTop.Size = new System.Drawing.Size(592, 426);
+			this.pnlTop.Size = new System.Drawing.Size(612, 426);
 			this.pnlTop.TabIndex = 0;
 			// 
 			// gbTerrains
@@ -151,7 +151,7 @@
 			this.gbTerrains.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.gbTerrains.Location = new System.Drawing.Point(0, 110);
 			this.gbTerrains.Name = "gbTerrains";
-			this.gbTerrains.Size = new System.Drawing.Size(592, 316);
+			this.gbTerrains.Size = new System.Drawing.Size(612, 316);
 			this.gbTerrains.TabIndex = 2;
 			this.gbTerrains.TabStop = false;
 			this.gbTerrains.Text = "Terrains";
@@ -172,7 +172,7 @@
 			this.lbTerrainsAvailable.Dock = System.Windows.Forms.DockStyle.Right;
 			this.lbTerrainsAvailable.FormattingEnabled = true;
 			this.lbTerrainsAvailable.ItemHeight = 12;
-			this.lbTerrainsAvailable.Location = new System.Drawing.Point(325, 45);
+			this.lbTerrainsAvailable.Location = new System.Drawing.Point(345, 45);
 			this.lbTerrainsAvailable.Name = "lbTerrainsAvailable";
 			this.lbTerrainsAvailable.Size = new System.Drawing.Size(264, 268);
 			this.lbTerrainsAvailable.TabIndex = 2;
@@ -186,7 +186,7 @@
 			this.pnlTerrainsHeader.Dock = System.Windows.Forms.DockStyle.Top;
 			this.pnlTerrainsHeader.Location = new System.Drawing.Point(3, 15);
 			this.pnlTerrainsHeader.Name = "pnlTerrainsHeader";
-			this.pnlTerrainsHeader.Size = new System.Drawing.Size(586, 30);
+			this.pnlTerrainsHeader.Size = new System.Drawing.Size(606, 30);
 			this.pnlTerrainsHeader.TabIndex = 0;
 			// 
 			// lblTerrainChanges
@@ -194,7 +194,7 @@
 			this.lblTerrainChanges.Dock = System.Windows.Forms.DockStyle.Top;
 			this.lblTerrainChanges.Location = new System.Drawing.Point(0, 0);
 			this.lblTerrainChanges.Name = "lblTerrainChanges";
-			this.lblTerrainChanges.Size = new System.Drawing.Size(586, 15);
+			this.lblTerrainChanges.Size = new System.Drawing.Size(606, 15);
 			this.lblTerrainChanges.TabIndex = 0;
 			this.lblTerrainChanges.Text = "Changes to terrains take effect immediately.";
 			this.lblTerrainChanges.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -225,15 +225,15 @@
 			this.pnlSpacer.Controls.Add(this.btnMoveDown);
 			this.pnlSpacer.Controls.Add(this.btnMoveRight);
 			this.pnlSpacer.Controls.Add(this.btnMoveUp);
-			this.pnlSpacer.Location = new System.Drawing.Point(270, 45);
+			this.pnlSpacer.Location = new System.Drawing.Point(280, 45);
 			this.pnlSpacer.Name = "pnlSpacer";
-			this.pnlSpacer.Size = new System.Drawing.Size(55, 170);
+			this.pnlSpacer.Size = new System.Drawing.Size(55, 160);
 			this.pnlSpacer.TabIndex = 3;
 			// 
 			// btnTerrainPaste
 			// 
 			this.btnTerrainPaste.Anchor = System.Windows.Forms.AnchorStyles.Top;
-			this.btnTerrainPaste.Location = new System.Drawing.Point(5, 140);
+			this.btnTerrainPaste.Location = new System.Drawing.Point(5, 135);
 			this.btnTerrainPaste.Name = "btnTerrainPaste";
 			this.btnTerrainPaste.Size = new System.Drawing.Size(45, 25);
 			this.btnTerrainPaste.TabIndex = 5;
@@ -244,7 +244,7 @@
 			// btnTerrainCopy
 			// 
 			this.btnTerrainCopy.Anchor = System.Windows.Forms.AnchorStyles.Top;
-			this.btnTerrainCopy.Location = new System.Drawing.Point(5, 115);
+			this.btnTerrainCopy.Location = new System.Drawing.Point(5, 110);
 			this.btnTerrainCopy.Name = "btnTerrainCopy";
 			this.btnTerrainCopy.Size = new System.Drawing.Size(45, 25);
 			this.btnTerrainCopy.TabIndex = 4;
@@ -256,7 +256,7 @@
 			// 
 			this.btnMoveLeft.Anchor = System.Windows.Forms.AnchorStyles.Top;
 			this.btnMoveLeft.Enabled = false;
-			this.btnMoveLeft.Location = new System.Drawing.Point(5, 5);
+			this.btnMoveLeft.Location = new System.Drawing.Point(5, 0);
 			this.btnMoveLeft.Name = "btnMoveLeft";
 			this.btnMoveLeft.Size = new System.Drawing.Size(45, 25);
 			this.btnMoveLeft.TabIndex = 0;
@@ -268,7 +268,7 @@
 			// 
 			this.btnMoveDown.Anchor = System.Windows.Forms.AnchorStyles.Top;
 			this.btnMoveDown.Enabled = false;
-			this.btnMoveDown.Location = new System.Drawing.Point(5, 80);
+			this.btnMoveDown.Location = new System.Drawing.Point(5, 75);
 			this.btnMoveDown.Name = "btnMoveDown";
 			this.btnMoveDown.Size = new System.Drawing.Size(45, 25);
 			this.btnMoveDown.TabIndex = 3;
@@ -280,7 +280,7 @@
 			// 
 			this.btnMoveRight.Anchor = System.Windows.Forms.AnchorStyles.Top;
 			this.btnMoveRight.Enabled = false;
-			this.btnMoveRight.Location = new System.Drawing.Point(5, 30);
+			this.btnMoveRight.Location = new System.Drawing.Point(5, 25);
 			this.btnMoveRight.Name = "btnMoveRight";
 			this.btnMoveRight.Size = new System.Drawing.Size(45, 25);
 			this.btnMoveRight.TabIndex = 1;
@@ -292,7 +292,7 @@
 			// 
 			this.btnMoveUp.Anchor = System.Windows.Forms.AnchorStyles.Top;
 			this.btnMoveUp.Enabled = false;
-			this.btnMoveUp.Location = new System.Drawing.Point(5, 55);
+			this.btnMoveUp.Location = new System.Drawing.Point(5, 50);
 			this.btnMoveUp.Name = "btnMoveUp";
 			this.btnMoveUp.Size = new System.Drawing.Size(45, 25);
 			this.btnMoveUp.TabIndex = 2;
@@ -312,7 +312,7 @@
 			this.gbTileset.Dock = System.Windows.Forms.DockStyle.Top;
 			this.gbTileset.Location = new System.Drawing.Point(0, 50);
 			this.gbTileset.Name = "gbTileset";
-			this.gbTileset.Size = new System.Drawing.Size(592, 60);
+			this.gbTileset.Size = new System.Drawing.Size(612, 60);
 			this.gbTileset.TabIndex = 1;
 			this.gbTileset.TabStop = false;
 			this.gbTileset.Text = "Tileset";
@@ -320,7 +320,7 @@
 			// btnCreateMap
 			// 
 			this.btnCreateMap.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-			this.btnCreateMap.Location = new System.Drawing.Point(505, 30);
+			this.btnCreateMap.Location = new System.Drawing.Point(525, 30);
 			this.btnCreateMap.Name = "btnCreateMap";
 			this.btnCreateMap.Size = new System.Drawing.Size(50, 25);
 			this.btnCreateMap.TabIndex = 5;
@@ -332,7 +332,7 @@
 			// btnFindDirectory
 			// 
 			this.btnFindDirectory.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-			this.btnFindDirectory.Location = new System.Drawing.Point(560, 10);
+			this.btnFindDirectory.Location = new System.Drawing.Point(580, 10);
 			this.btnFindDirectory.Name = "btnFindDirectory";
 			this.btnFindDirectory.Size = new System.Drawing.Size(25, 20);
 			this.btnFindDirectory.TabIndex = 2;
@@ -352,7 +352,7 @@
 			// btnFindTileset
 			// 
 			this.btnFindTileset.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-			this.btnFindTileset.Location = new System.Drawing.Point(560, 30);
+			this.btnFindTileset.Location = new System.Drawing.Point(580, 30);
 			this.btnFindTileset.Name = "btnFindTileset";
 			this.btnFindTileset.Size = new System.Drawing.Size(25, 25);
 			this.btnFindTileset.TabIndex = 6;
@@ -366,7 +366,7 @@
 			| System.Windows.Forms.AnchorStyles.Right)));
 			this.lblPathCurrent.Location = new System.Drawing.Point(105, 15);
 			this.lblPathCurrent.Name = "lblPathCurrent";
-			this.lblPathCurrent.Size = new System.Drawing.Size(455, 15);
+			this.lblPathCurrent.Size = new System.Drawing.Size(475, 15);
 			this.lblPathCurrent.TabIndex = 1;
 			this.lblPathCurrent.Text = "lblPathCurrent";
 			this.lblPathCurrent.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -391,7 +391,7 @@
 			this.gbHeader.Dock = System.Windows.Forms.DockStyle.Top;
 			this.gbHeader.Location = new System.Drawing.Point(0, 0);
 			this.gbHeader.Name = "gbHeader";
-			this.gbHeader.Size = new System.Drawing.Size(592, 50);
+			this.gbHeader.Size = new System.Drawing.Size(612, 50);
 			this.gbHeader.TabIndex = 0;
 			this.gbHeader.TabStop = false;
 			this.gbHeader.Text = "Maptree";
@@ -401,7 +401,7 @@
 			this.lblTilesetCurrent.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
 			this.lblTilesetCurrent.Font = new System.Drawing.Font("Verdana", 7F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.lblTilesetCurrent.ForeColor = System.Drawing.SystemColors.Highlight;
-			this.lblTilesetCurrent.Location = new System.Drawing.Point(415, 30);
+			this.lblTilesetCurrent.Location = new System.Drawing.Point(435, 30);
 			this.lblTilesetCurrent.Name = "lblTilesetCurrent";
 			this.lblTilesetCurrent.Size = new System.Drawing.Size(170, 15);
 			this.lblTilesetCurrent.TabIndex = 5;
@@ -411,7 +411,7 @@
 			// lblHeaderTileset
 			// 
 			this.lblHeaderTileset.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-			this.lblHeaderTileset.Location = new System.Drawing.Point(535, 15);
+			this.lblHeaderTileset.Location = new System.Drawing.Point(555, 15);
 			this.lblHeaderTileset.Name = "lblHeaderTileset";
 			this.lblHeaderTileset.Size = new System.Drawing.Size(50, 15);
 			this.lblHeaderTileset.TabIndex = 4;
@@ -424,7 +424,7 @@
 			| System.Windows.Forms.AnchorStyles.Right)));
 			this.lblGroupCurrent.Location = new System.Drawing.Point(90, 15);
 			this.lblGroupCurrent.Name = "lblGroupCurrent";
-			this.lblGroupCurrent.Size = new System.Drawing.Size(320, 15);
+			this.lblGroupCurrent.Size = new System.Drawing.Size(340, 15);
 			this.lblGroupCurrent.TabIndex = 1;
 			this.lblGroupCurrent.Text = "lblGroupCurrent";
 			// 
@@ -442,7 +442,7 @@
 			| System.Windows.Forms.AnchorStyles.Right)));
 			this.lblCategoryCurrent.Location = new System.Drawing.Point(90, 30);
 			this.lblCategoryCurrent.Name = "lblCategoryCurrent";
-			this.lblCategoryCurrent.Size = new System.Drawing.Size(320, 15);
+			this.lblCategoryCurrent.Size = new System.Drawing.Size(340, 15);
 			this.lblCategoryCurrent.TabIndex = 3;
 			this.lblCategoryCurrent.Text = "lblCategoryCurrent";
 			// 
@@ -457,14 +457,14 @@
 			// 
 			this.AutoScaleBaseSize = new System.Drawing.Size(5, 12);
 			this.CancelButton = this.btnCancel;
-			this.ClientSize = new System.Drawing.Size(592, 456);
+			this.ClientSize = new System.Drawing.Size(612, 456);
 			this.Controls.Add(this.pnlTop);
 			this.Controls.Add(this.pnlBottom);
 			this.Font = new System.Drawing.Font("Verdana", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
 			this.MaximizeBox = false;
 			this.MinimizeBox = false;
-			this.MinimumSize = new System.Drawing.Size(400, 350);
+			this.MinimumSize = new System.Drawing.Size(480, 386);
 			this.Name = "MapTreeTilesetInputBox";
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
 			this.pnlBottom.ResumeLayout(false);
